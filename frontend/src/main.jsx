4154e540044e71bc5d72c265ee2e435029268252
Chirag <chiragdhigan001@gmail.com>
2026-05-20 15:44:39 +0530

    Uncomment React imports and rendering code
    Uncommented the import statements and the createRoot rendering function for the React application.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,3 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-// import { BrowserRouter } from "react-router-dom";
-// import { AppContextProvider } from "./context/AppContext.jsx";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
-
-// createRoot(document.getElementById("root")).render(
-//   <BrowserRouter>
-//     <GoogleOAuthProvider
-//       clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-//     ></GoogleOAuthProvider>
-//     <AppContextProvider>
-//       <App />
-//     </AppContextProvider>
-//   </BrowserRouter>,
-// );
-
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
