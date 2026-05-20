b7dd3a4c5d8abdcdc2f39bee3a74af878a00f937
Chirag <chiragdhigan001@gmail.com>
2026-05-20 15:18:39 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,40 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+// import { AppContextProvider } from "./context/AppContext.jsx";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <GoogleOAuthProvider
+//       clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+//     ></GoogleOAuthProvider>
+//     <AppContextProvider>
+//       <App />
+//     </AppContextProvider>
+//   </BrowserRouter>,
+// );
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <GoogleOAuthProvider
       clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-    ></GoogleOAuthProvider>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>,
+    >
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </GoogleOAuthProvider>
+  </BrowserRouter>
 );
