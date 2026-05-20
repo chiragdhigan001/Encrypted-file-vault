142b9a3df0c4356806234f1093a9bd96e4df739e
Chirag <chiragdhigan001@gmail.com>
2026-05-20 15:13:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,12 @@ import logger from "./utils/logger.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+app.set("trust proxy", 1); // fixed
 // DEBUG
 console.log("GOOGLE CLIENT ID:", process.env.GOOGLE_CLIENT_ID);
 connectDb();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174']
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174',"https://encrypted-file-vault.vercel.app"] //fixed
 app.use(cors({origin: allowedOrigins,credentials: true}));
 app.disable("x-powered-by");
 app.use(requestContext);
